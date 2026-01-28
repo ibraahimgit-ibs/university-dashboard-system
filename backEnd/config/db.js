be416c625acab91a9c2882e799a8f91f474b681e
ibraahimgit-ibs <ibraahimgit@gmail.com>
2026-01-28 22:03:30 +0300

    Update

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -3,23 +3,21 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-const { Pool } = pg
+const { Pool } = pg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASS
-});
-
-// pool.query('SELECT * FROM students', (err, res) => {
-//   if (err) {
-//     console.error('Error connecting to DB:', err)
-//   } else {
-//     console.log('Connected! Current time:', res.rows)
-//   }
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASS
 // });
+
+const pool = new Pool( {
+  connectionString: process.env.DATABASE_URL, ssl: {
+    rejectUnauthorized: false, // Render requires SSL 
+  },
+} );
 
 
 export default pool;
--- dashboard_system/vite.config.js
+++ dashboard_system/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig( {
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://university-dashboard-system.onrender.com',
+        // target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
