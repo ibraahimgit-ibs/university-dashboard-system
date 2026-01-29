d3df8fe9b3047854f1282c43f973b0cf37916055
ibraahimgit-ibs <ibraahimgit@gmail.com>
2026-01-29 12:58:55 +0300

    Update

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -14,10 +14,15 @@ const { Pool } = pg;
 // });
 
 const pool = new Pool( {
-  connectionString: process.env.DATABASE_URL, ssl: {
-    rejectUnauthorized: false, // Render requires SSL 
-  },
+    connectionString: process.env.DATABASE_URL, ssl: {
+        rejectUnauthorized: false, // Render requires SSL 
+    },
 } );
+
+pool.query( 'SELECT NOW()' )
+    .then( res => console.log( 'Connected:', res.rows[0] ) )
+    .catch( err => console.error( 'DB connection error:', err ) );
+
 
 
 export default pool;
