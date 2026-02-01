6cbc1ac280fd41a8cf5f247a3c08344f1372f306
ibraahimgit-ibs <ibraahimgit@gmail.com>
2026-02-01 11:34:50 +0300

    Update

--- backEnd/server.js
+++ backEnd/server.js
@@ -32,7 +32,6 @@ app.get( "/api/check-auth", authenticate, ( req, res ) => {
 
 // **********logout**********//
 app.post( "/api/logout", ( req, res ) => {
-  res.clearCookie( "token" );
   res.status( 200 ).json( { message: "Logged out successfully" } );
 } );
 // -------------------------
--- dashboard_system/src/App.jsx
+++ dashboard_system/src/App.jsx
@@ -28,8 +28,8 @@ function App() {
   useEffect( () => {
     async function fetchData() {
       try {
-        const respons = await axios.get( `https://university-dashboard-system.onrender.com/api/student/student-data`, {withCredentials: true,} );
-        setStudentData( respons.data ); { withCredentials: true,}
+        const respons = await axios.get( `https://university-dashboard-system.onrender.com/api/student/student-data`, {withCredentials: true} );
+        setStudentData( respons.data );
       } catch ( err ) {
         console.error( err );
       }
